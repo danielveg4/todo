627d619aeb32c0bc40e8a8b11983ed58d1856d1c
danielveg4 <dvgilles@gmail.com>
2024-08-23 18:56:46 +0200

    fixed rps

--- paperrockscissors/script.js
+++ paperrockscissors/script.js
@@ -1,88 +1,91 @@
-/* PIEDRA, PAPEL O TIJERA
+const userPointsElement = document.getElementById('user-points');
+const pcPointsElement = document.getElementById('pc-points');
+const gameOptionsElement = document.getElementById('game-options');
+const userMoveElement = document.getElementById('user-move');
+const pcMoveElement = document.getElementById('pc-move');
+const resultElement = document.getElementById('result');
 
-1. HTML y CSS: header y marcador, botones de piedra-papel-tijera
-2. Leer los botones y el marcador
-3. Jugadas posibles
-4. Leer click
-5. Jugada aleatoria del PC
-6. Mostrar ganador
-7. Sumar puntos al marcador
+let userMove = null;
+let pcMove = null;
+let userPoints = 0;
+let pcPoints = 0;
 
-*/
-
-const userPointsElement = document.getElementById('point-user');
-const pcPointsElement = document.getElementById('point-pc');
-const gameItemsElement = document.getElementById('game-items');
-const userPickedElement = document.getElementById('result-user');
-const pcPickedElement = document.getElementById('result-pc');
-const textResultElement = document.getElementById('result');
-
-
-
-gameItemsElement.addEventListener('click', (event) => {
-    console.log(event.target.dataset.item)
-})
-
-let userSelection = null;
-let pcSelection = null;
-
-let userContador = 0;
-let pcContador = 0;
-
-const pcMove = () => {
-    const randomNumberPcMove = Math.floor(Math.random() * 3) + 1;
-    let pcMoveRound;
-    if (randomNumberPcMove === 1) {
-        pcMoveRound = 'papel';
-        console.log(pcMoveRound);
-    } else if (randomNumberPcMove === 2){
-        pcMoveRound = 'piedra';
-        console.log(pcMoveRound);
-    } else if (randomNumberPcMove === 3){
-        pcMoveRound = 'tijera';
-        console.log(pcMoveRound)
+const validMoves = ['rock', 'paper', 'scissor'];
+const gameRules = {
+    paper: {
+        rock: true,
+        scissor: false,
+        spock: false,
+        lizzard: true
+    },
+    rock: {
+        paper: false,
+        scissor: true,
+        spock: true,
+        lizzard: false
+    },
+    scissor: {
+        paper: true,
+        rock: false,
+        spock: true,
+        lizzard: false
+    },
+    lizzard: {
+        paper: true,
+        rock: true,
+        scissor: false,
+        spock: false
+    },
+    spock: {
+        paper: true,
+        rock: false,
+        scissor: true,
+        lizzard: false
     }
-    return pcMoveRound;
+};
+
+
+const setPcMove = () => {
+    const randomPcNumber = Math.floor(Math.random() * validMoves.length);
+    pcMove = validMoves[randomPcNumber];
 }
 
-const userMove = (event) => {
-    const validMoves = ['piedra', 'papel', 'tijera'];
-
-    if (validMoves.includes(event.target.dataset.item)) {
-        userMoveRound = event.target.dataset.item;
-        console.log(`${event.target.dataset.item}`);
-        return userMoveRound;
-    } else {
-        console.log('Movimiento inválido');
-        return null; 
+const setUserMove = event => {
+    if (!event.target.dataset.item) {
+        return;
     }
+    userMove = event.target.dataset.item;
+    setPcMove();
+
 }
 
-const resultMove = (event) => {
-    const userMoveRound = userMove(event); 
-    const pcMoveRound = pcMove();
-    if (userMoveRound === pcMoveRound) {
-        console.log('empate');
-        textResultElement.textContent = 'Empate';
-    } else if (
-        (userMoveRound === 'piedra' && pcMoveRound === 'tijera') ||
-        (userMoveRound === 'tijera' && pcMoveRound === 'papel') ||
-        (userMoveRound === 'papel' && pcMoveRound === 'piedra')
-    ) {
-        textResultElement.textContent = 'El usuario ha ganado';
-        userContador++;
-        userPointsElement.textContent = userContador;
-        
-    } else if (userMoveRound === null) {
-        console.log('null') 
-    } else {
-        textResultElement.textContent = 'El PC ha ganado';
-        pcContador++;
-        pcPointsElement.textContent = pcContador;
+const setMove = (event) => {
+    setUserMove(event);
+    if (userMove === pcMove) {
+        resultElement.textContent = 'TIE'
+    } else if (gameRules[userMove][pcMove] === true) {
+        userPoints++;
+        resultElement.textContent = 'YOU WIN'
+    } else if (gameRules[userMove][pcMove] === false) {
+        pcPoints++;
+        resultElement.textContent = 'PC WIN'
     }
 }
 
+const printMove = (event) => {
+    setMove(event);
+    userPointsElement.textContent = userPoints;
+    pcPointsElement.textContent = pcPoints;
+    userMoveElement.textContent = userMove;
+    pcMoveElement.textContent = pcMove;    
+}
 
-gameItemsElement.addEventListener('click', resultMove);
+
+if (document.body.dataset.mode === 'advanced') {
+    validMoves.push('lizzard', 'spock')
+}
+
+
+gameOptionsElement.addEventListener('click', printMove);
 
 
